1eb094a678d9219d99285b0d84f87ccce9e041d2
Richard Feldman <richard.t.feldman@gmail.com>
2018-04-12 11:03:16 +0200

    Update part2 comments.

--- part2/Main.elm
+++ part2/Main.elm
@@ -13,6 +13,21 @@ searchResults =
     ]
 
 
+viewSearchResult searchResult =
+    -- This function will receive `searchResult`, one of the strings
+    -- in the list above.
+    li []
+        [-- TODO use `searchResult` to put a link here that points to
+         -- something like this:
+         --
+         -- https://github.com/TheSeamau5/elm-checkerboardgrid-tutorial
+         --
+         -- by prepending "https://github.com/" to the searchResult string
+         --
+         -- HINT: This will also involve using parentheses!
+        ]
+
+
 main =
     let
         elmHubHeader =
@@ -24,21 +39,9 @@ main =
     div [ class "content" ]
         [ text "TODO put the contents of elmHubHeader here instead of this text!"
         , ul [ class "results" ]
-            -- TODO replace this [] with a `List.map` to turn `searchResults` into some Html!
+            -- TODO replace this [] with a `List.map` which uses the
+            -- `viewSearchResult` function to turn `searchResults` into some Html!
             --
             -- HINT: You'll need some parentheses to do this!
             []
-        ]
-
-
-viewSearchResult searchResult =
-    li []
-        [-- TODO use `searchResult` to put a link here that points to
-         -- something like this:
-         --
-         -- https://github.com/TheSeamau5/elm-checkerboardgrid-tutorial
-         --
-         -- by prepending "https://github.com/" to the searchResult string
-         --
-         -- HINT: This will also involve using parentheses!
         ]
